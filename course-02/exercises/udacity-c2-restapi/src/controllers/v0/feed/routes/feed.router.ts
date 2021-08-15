49026122d8d0d9c1e2447f7dd530b5a60997c883
Georgios Antonopoulos <georanto@gmail.com>
2021-08-15 20:42:49 +0200

    Implements the Lesson 4.4 todos.

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,43 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id',
+    async (req: Request, res: Response) => {
+        let {id} = req.params;
+        if (!id) {
+            return res.status(400).send("Id is required");
+        }
+
+        const result = await FeedItem.findOne({where: {id: id}})
+        if (!result) {
+            return res.status(404).send(`No entry with id$=${id} was found!`);    
+        }
+        
+        return res.status(200).send(result);
+    });
+
+
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        let {id} = req.params;
+        if (!id) {
+            return res.status(400).send("Id is required");
+        }
+
+        const result = await FeedItem.findOne({where: {id: id}});
+        if (!result) {
+            return res.status(404).send(`No entry with id$=${id} was found!`);
+        }
+
+        await result.update({caption: "Changed"}).then(() => {
+            return res.status(200).send(result);
+        }, () => {
+            return res.status(500).send(result);
+        });
 });
 
 
