4f33e1bc4120f3358e3fb05ebe6ab22a8002072d
Georgios Antonopoulos <georanto@gmail.com>
2021-08-16 08:55:30 +0200

    Configures an .env based credential system

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,12 +1,12 @@
 export const config = {
   "dev": {
-    "username": "pontikasdb",
-    "password": "t3fwrsidDiJ6vRP",
+    "username": process.env["POSTGRES_USERNAME"],
+    "password": process.env["POSTGRES_PASSWORD"],
     "database": "pontikasdb",
     "host": "pontikasdb.crtkvdvwlnm8.eu-west-3.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "eu-west-3",
-    "aws_profile": "default",
+    "aws_region": process.env["AWS_REGION"],
+    "aws_profile": process.env["AWS_PROFILE"],
     "aws_media_bucket": "pontikasdb-bucky-dev"
   },
   "prod": {
