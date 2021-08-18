093c56eeced0ef62c2e8193f4be7d9562c4404ae
Georgios Antonopoulos <georanto@gmail.com>
2021-08-18 23:26:07 +0200

    - Sets the correct sequelize package revision. - Set further environment variables

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -2,12 +2,12 @@ export const config = {
   "dev": {
     "username": process.env["POSTGRES_USERNAME"],
     "password": process.env["POSTGRES_PASSWORD"],
-    "database": "pontikasdb",
-    "host": "pontikasdb.crtkvdvwlnm8.eu-west-3.rds.amazonaws.com",
+    "database": process.env["POSTGRES_DATABASE"],
+    "host": process.env["POSTGRES_HOST"],
     "dialect": "postgres",
     "aws_region": process.env["AWS_REGION"],
     "aws_profile": process.env["AWS_PROFILE"],
-    "aws_media_bucket": "pontikasdb-bucky-dev"
+    "aws_media_bucket": process.env["AWS_MEDIA_BUCKET"]
   },
   "prod": {
     "username": "",
