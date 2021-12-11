8922ba299a56cd50a2232c68a10cd4f1e8654cf9
Georgios Antonopoulos <georanto@gmail.com>
2021-12-11 17:56:04 +0100

    - Makes a changes so the code compiles. - Adds the correct region.

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -9,14 +9,11 @@ import { getUserId } from '../utils';
 
 // TODO: Get all TODO items for a current user
 export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
     const todos = '...'
 
     return undefined
-
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+    })
+    
+handler.use(cors({credentials: true}))
--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'gantonopoulos.eu.auth0.com',            // Auth0 domain
