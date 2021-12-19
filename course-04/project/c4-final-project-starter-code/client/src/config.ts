a8fd1487cb0978b8ceeb5231a0ea1be9cf4deb59
Georgios Antonopoulos <georanto@gmail.com>
2021-12-19 22:49:54 +0100

    Update the client data

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '6ikhv1d1ii'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'bxwgip8xvg'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
